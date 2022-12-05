6a3c3c2e4dc5cef8d80d3c7e9633f5a0635b3632
george_pantelimon <80425896+gigicu01@users.noreply.github.com>
2022-12-05 16:01:18 -0500

    SignUp/Signout Pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,43 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SignUpPage } from './pages/signupPage';
 import { Container } from '@material-ui/core'
+import SignInPage from './pages/signInPage';
+import { SignOutPage } from './pages/signOutPage';
+import { supabaseClient } from "./api/supabaseClient"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element = { <SignUpPage /> } />
-        <Route path="/signin" />
-        <Route path="/signout" />
-        <Route path="/"/>
+        <Route path="/signin" element = { <SignInPage /> } />
+        <Route path="/signout" element = { <SignOutPage /> } />
+        <Route path="/" element = {  <HomePage />} />
       </Routes>
     </Router>
   );
+}
+
+// const stuff = async () => {
+//   const { data } = await supabaseClient.auth.getSession()
+//   return <div>
+//     {data.session && `you are logged in as ${data.session.user.email}`}
+//   </div>
+// }
+
+const Hoempage = () => {
+  const whatever = supabaseClient.auth.getSession().then(res => {
+      this.setState({
+        session: res.data.session?.user.email
+      })
+      const data = res.data
+      return res.data
+    })
+    
+   return <div>
+     {whatever.then && `you are logged in as ${data.session.user.email}`}
+   </div>    
+    
 }
 
 export default App;
