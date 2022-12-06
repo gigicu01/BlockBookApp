4ec000b8be37854d9af3963eb3b4a6eb63b50113
E0MCH1NA <ilu23@jcu.edu>
2022-12-06 15:14:20 -0500

    post page

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -3,36 +3,29 @@ import { Link } from 'react-router-dom';
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from '@material-ui/core/Button';
-
+import { supabaseClient } from "../api/supabaseClient";
+//import imageToAdd from "./public/bb_logo.png";
 
 function Post(){ //got this navbar stuff also from The Net Ninja
     return ( 
-        <form>
-          <div>
-            <TextField
-              name="username"
-              label="Username"
-              type="text"
-              autoComplete="Username"
-              variant="outlined"
-            />
-          </div>
-          <div>
-            <TextField
-              name="postContent"
-              label="Post Content"
-              type="text"
-              autoComplete="Post Content"
-              variant="outlined"
-            />
-          </div>
-    
-          <Button type="submit">
-              Post
-            </Button>
-        </form>
-        
-            
+      <div className ="postbox">
+        <div className="postWrapper">
+          <form>
+          <img src="bb_logo.png" alt="BlockBook Logo" height="60"/>
+          <img src="bb_logo.png" alt="Profile Picture" height="60" className = "profilepic"/>
+            <div>
+                <input type="text" name="topic" className="topic" placeholder='Topic' />
+            </div>
+            <div>
+                <input type="text" name="postContents" className="postContent" placeholder="What Do You Wanna Say?" />
+            </div>
+      
+            <Button type="submit">
+                Post
+              </Button>
+          </form>
+        </div>
+      </div>     
         
     )
 }
